f18997329c30c6752b8a8f91cafb6ff34b521ae0
Narendran Omprakash <48692176+Naren2002@users.noreply.github.com>
2022-12-09 19:58:13 +0530

    Added a URL shortener endpoint /api/shorturl

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,8 +11,36 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+let mappingKey = 0;
+
+let ogToShortMap = {
+
+}
+
+let shortToOgMap = {
+
+}
+
+app.post("/api/shorturl", function(req, res){
+    let inputUrl = req.body.url;
+    console.log(inputUrl);
+    if (!(inputUrl in ogToShortMap)){
+      mappingKey += 1;
+      ogToShortMap[inputUrl] = mappingKey;
+      shortToOgMap[mappingKey] = inputUrl;
+
+      res.json({
+        original_url: inputUrl,
+        short_url: mappingKey
+      })
+    }
 });
 
 // Your first API endpoint
